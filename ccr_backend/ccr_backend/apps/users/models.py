129eb02f9a6ca267a28c5594bfd7fb2c8b7c8e66
지찬규 <ckj9014@gmail.com>
2019-12-16 03:53:30 +0900

    [apps] UserGitHub 모델 추가 - git contribution count 및 address 저장 모델

--- ccr_backend/ccr_backend/apps/users/models.py
+++ ccr_backend/ccr_backend/apps/users/models.py
@@ -12,3 +12,16 @@ class User(auth_models.AbstractUser):
 
     def __str__(self):
         return "name: {}".format(self.username)
+
+
+class UserGitHub(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    address = models.CharField(max_length=100, null=False, blank=False)
+    count = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'user_github'
+        verbose_name_plural = 'user_github'
+
+    def __str__(self):
+        return "name: {}/ address='{}'".format(self.user.username, self.address)
