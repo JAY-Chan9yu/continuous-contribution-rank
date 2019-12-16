b6a0ddc02e9abdfb5a1469abed476eab2efbdc55
지찬규 <ckj9014@gmail.com>
2019-12-16 21:27:37 +0900

    [api] github oauth 인증 url 응답 api 추가

--- ccr_backend/ccr_backend/api/users/urls.py
+++ ccr_backend/ccr_backend/api/users/urls.py
@@ -3,10 +3,11 @@ from __future__ import unicode_literals
 
 from django.conf.urls import url
 
-from api.users.views import UserInformationViewSet, UserRegisterGitHubViewSet
+from api.users.views import UserInformationViewSet, UserRegisterGitHubViewSet, GitHubLoginViewSet
 
 # TODO: view method define 정리하기
 urlpatterns = [
     url(r'^inform/$', UserInformationViewSet.as_view({'get': 'list', 'post': 'create'}), name='information'),
-    url(r'^(?P<user_id>\d+)/github/$', UserRegisterGitHubViewSet.as_view({'get': 'list', 'post': 'create'}), name='github'),
+    url(r'^(?P<user_id>\d+)/github/$', UserRegisterGitHubViewSet.as_view({'get': 'list', 'post': 'create'}), name='user_github'),
+    url(r'^github/$', GitHubLoginViewSet.as_view({'get': 'list', 'post': 'create'}), name='github'),
 ]
--- ccr_backend/ccr_backend/api/users/views.py
+++ ccr_backend/ccr_backend/api/users/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework import viewsets
 from rest_framework.response import Response
 
@@ -40,3 +41,25 @@ class UserRegisterGitHubViewSet(viewsets.ModelViewSet, viewsets.ViewSetMixin):
     def perform_create(self, serializer):
         result = serializer.save()
         return result
+
+
+class GitHubLoginViewSet(viewsets.ModelViewSet, viewsets.ViewSetMixin):
+    """
+        User 의 github 주소, contribution count 관한 api
+    """
+    queryset = UserGitHub.objects.all()
+    serializer_class = UserGitHubSerializer
+
+    def list(self, request, *args, **kwargs):
+        # TODO : query 정리해서 깔끔하게 하기
+        rl = 'https://github.com/login/oauth/authorize?' \
+             'redirect_uri={}&' \
+             'client_id={}&' \
+             'scope={}'.format(settings.GITHUB_HOST,
+                               settings.GITHUB_CLIENT_ID,
+                               'user:email')
+        # vue.js 에서 cors 피하기 위해 header에 정의해줘야 한다.
+        response = Response(rl, content_type='application/json')
+        response._headers['Access-Control-Allow-Origin'] = ('Access-Control-Allow-Origin', '*')
+
+        return response
