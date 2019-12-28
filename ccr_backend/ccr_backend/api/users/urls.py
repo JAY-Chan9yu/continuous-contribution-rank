3f4c49cf35cd5d465acfed65167b7ad7a5b37725
지찬규 <ckj9014@gmail.com>
2019-12-28 18:27:06 +0900

    [api] user url 정리

--- ccr_backend/ccr_backend/api/users/urls.py
+++ ccr_backend/ccr_backend/api/users/urls.py
@@ -5,9 +5,20 @@ from django.conf.urls import url
 
 from api.users.views import UserInformationViewSet, UserRegisterGitHubViewSet, GitHubLoginViewSet
 
-# TODO: view method define 정리하기
+information = UserInformationViewSet.as_view({
+    'get': 'list', 'post': 'create'
+})
+
+user_github = UserRegisterGitHubViewSet.as_view({
+    'get': 'list', 'post': 'create'
+})
+
+github = GitHubLoginViewSet.as_view({
+    'get': 'list', 'post': 'create'
+})
+
 urlpatterns = [
-    url(r'^inform/$', UserInformationViewSet.as_view({'get': 'list', 'post': 'create'}), name='information'),
-    url(r'^(?P<user_id>\d+)/github/$', UserRegisterGitHubViewSet.as_view({'get': 'list', 'post': 'create'}), name='user_github'),
-    url(r'^github/$', GitHubLoginViewSet.as_view({'get': 'list', 'post': 'create'}), name='github'),
+    url(r'^inform/$', information, name='information'),
+    url(r'^(?P<user_id>\d+)/github/$', user_github, name='user_github'),
+    url(r'^github/$', github, name='github'),
 ]
